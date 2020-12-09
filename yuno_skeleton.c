412156c3d902e8c3b730b427bd33566008a0beb1
Niyamaka <niyamaka@yuneta.io>
2020-12-09 01:04:00 +0100

    permission as trace level

--- yuno_skeleton.c
+++ yuno_skeleton.c
@@ -18,7 +18,7 @@
  *      Constants
  ***************************************************************************/
 #define NAME            "yuno-skeleton"
-#define APP_VERSION     "4.2.26"
+#define APP_VERSION     "4.2.27"
 #define APP_DATETIME    __DATE__ " " __TIME__
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 #define DEFAULT_SKELETON_PATH "/yuneta/development/bin/skeletons"
