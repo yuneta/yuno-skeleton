29cdd489472a744c8021a2a66891bf378ce94d82
Niyamaka <niyamaka@yuneta.io>
2021-06-15 17:09:04 +0200

    v4.13.3

--- yuno_skeleton.c
+++ yuno_skeleton.c
@@ -18,7 +18,7 @@
  *      Constants
  ***************************************************************************/
 #define NAME            "yuno-skeleton"
-#define APP_VERSION     "4.13.2"
+#define APP_VERSION     "4.13.3"
 #define APP_DATETIME    __DATE__ " " __TIME__
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 #define DEFAULT_SKELETON_PATH "/yuneta/development/bin/skeletons"
